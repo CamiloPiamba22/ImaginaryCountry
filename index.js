482c553e24a92acdfd31e4dea414e0677c9249c0
kvadrakola <kvadrakola@gmail.com>
2026-07-03 15:22:08 +0200

    feat: add rest of a code from w3school example

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
